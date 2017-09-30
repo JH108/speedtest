ef16545faf0d7a9c20f104abeb428c0866a82c7b
Jesse Hill <jsnahill@gmail.com>
2017-09-30 14:42:13 -0500

    Finish initial models for game data

--- speedtest/Model/GameDataModel.swift
+++ speedtest/Model/GameDataModel.swift
@@ -23,6 +23,4 @@ class GameData {
     func updateScore(score: Int) -> Void {
         self.currentScore = score
     }
-    
-    
 }
--- speedtest/Model/GridDataModel.swift
+++ speedtest/Model/GridDataModel.swift
@@ -11,4 +11,6 @@ import Foundation
 struct Grid {
     var numberOfColumns = 5
     var numberOfRows = 5
+    var lastNumber: Int = numberOfColumns * numberOfRows
+    var firstNumber = 1
 }
